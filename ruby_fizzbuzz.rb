82fac4ce2282611bbd5acea825e5233d8aaffec2
jeffspatafora <jeffspatafora@gmail.com>
2022-06-09 15:58:51 -0700

    adds ruby fizzbuzz problem work

---
+++ ruby_fizzbuzz.rb
@@ -0,0 +1,39 @@
+# Write a function that prints out every number from 1 to N, with the following exceptions:
+
+# If the number is divisible by 3, print out "FIZZ".
+# If the number is divisible by 5, print out "BUZZ".
+# If the number is divisible by both 3 and 5, print out "FIZZBUZZ".
+
+
+# array100 = []
+# number = 0
+# while number < 100
+#   number += 1
+#   array100 << number
+# end
+
+# p array100
+
+# array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
+
+def fizzbuzz(numbers_array)
+  new_numbers_array = []
+  i = 0
+  while i < numbers_array.length
+    if numbers_array[i] % 3 == 0 && numbers_array[i] % 5 == 0
+      new_numbers_array << "fizzbuzz"
+    elsif numbers_array[i] % 3 == 0
+      new_numbers_array << "fizz"
+    elsif numbers_array[i] % 5 == 0
+      new_numbers_array << "buzz"
+    else
+      new_numbers_array << numbers_array[i]
+    end
+    i += 1
+  end
+  new_numbers_array
+end
+
+p fizzbuzz([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100])
+
+
