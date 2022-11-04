6461b9e846cc2a476486600d2a9d93f6d474a1c2
jeffspatafora <jeffspatafora@gmail.com>
2022-11-03 21:39:57 -0700

    adds solution using while loop for array w/ positive numbers

--- ruby_skip_it.rb
+++ ruby_skip_it.rb
@@ -9,6 +9,24 @@
 # [2, 3, 1, 2, 2, 1, 5, 2, 2]
 
 # create new array
+# set index i to 0
+# while i is less than numbers array length, shovel numbers index i into new array
+# increment i by index i value of numbers array
+
+def skip_it(numbers)
+  new_numbers = []
+  i = 0
+  while i < numbers.length
+    new_numbers << numbers[i]
+    i += numbers[i]
+  end
+  p new_numbers
+end
+
+skip_it([2, 1, 3, 2, 5, 1, 2, 6, 2, 7, 1, 5, 6, 3, 2, 6, 2, 1, 2])
+# skip_it([-3, 1, 8, 1, -7, 2, 2, 4, 5])
+
+# create new array
 # add first number of old array to new array
 # create current_number_to_add variable
 # set current_number_to_add variable to be current number in original array
@@ -16,16 +34,16 @@
 # start loop to go until end of original array
 # add to new array << number_array[i + current_number_to_add]
 
-def skip_it(numbers_array)
-  new_numbers_array = []
-  new_numbers_array << numbers_array[0]
-  current_number_to_add = numbers_array[0]
-  i = 0
-  while i < numbers_array.length
-    new_numbers_array << numbers_array[i + current_number_to_add]
-    i += 
-  end
-  p new_numbers_array
-end
+# def skip_it(numbers_array)
+#   new_numbers_array = []
+#   new_numbers_array << numbers_array[0]
+#   current_number_to_add = numbers_array[0]
+#   i = 0
+#   while i < numbers_array.length
+#     new_numbers_array << numbers_array[i + current_number_to_add]
+#     i += 
+#   end
+#   p new_numbers_array
+# end
 
-skip_it([2, 1, 3, 2, 5, 1, 2, 6, 2, 7, 1, 5, 6, 3, 2, 6, 2, 1, 2])
+# skip_it([2, 1, 3, 2, 5, 1, 2, 6, 2, 7, 1, 5, 6, 3, 2, 6, 2, 1, 2])
